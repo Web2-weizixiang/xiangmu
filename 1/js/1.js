71b56cfe79f9ce59733b81a8ac28a135a4c49dbe
Web2-weizixiang <351253210@qq.com>
2017-08-10 21:14:59 +0800

    Add files via upload

---
+++ 1/js/1.js
@@ -0,0 +1,21 @@
+function clear(){
+ 		huantu1.style.backgroundColor="#B7B7B7";
+ 		huantu2.style.backgroundColor="#B7B7B7";
+ 		huantu3.style.backgroundColor="#B7B7B7";
+ 	}
+huantu1.onclick = function(){
+	clear();
+	banner.src = "img/banner1.png";
+	huantu1.style.backgroundColor="white";
+}
+huantu2.onclick = function(){
+	clear();
+	banner.src = "img/banner2.png";
+	huantu2.style.backgroundColor="white";	
+}
+huantu3.onclick = function(){
+	clear();
+	banner.src = "img/banner3.png";
+	huantu3.style.backgroundColor="white";
+}
+
